fb161ca924eaeea3711f5355afee9d6d85737ef7
esquivelio <daniel.esquivel3535@live.gmail.com>
2024-02-19 13:28:18 -0600

    style

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("hola ch 38 como andan?");
+// funcion que envie un saludo con alert 
+function mostrarAlerta (){
+alert ("Hola mundo");
+}
+/**
+ * pedir el nombre de la persona usando un prompt y mostrar un saludo personalizado 
+ */
+ function enviarSaludo(){
+let tuNombre = prompt("dime tu nombre");
+if tuNombre = null;
+alert("arre pues");
+alert("hola " + tuNombre + " Como estas??");
+
+ }
